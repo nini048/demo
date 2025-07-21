24b1982304c8fd30d16cde340015827e01824236
nini <bploveme048@gmail.com>
2025-07-21 17:44:03 +0700

    props

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UserInfor from "./Userinfor";
+import DisplayInfor from "./Displayinfor";
 class MyComponent extends React.Component {
     render() {
         return (
             <div>
-                
-                
                 <UserInfor/>
+                <br />
+                <br />
+                <DisplayInfor name= 'nini' age= {30}/>
             </div>
             
         );
